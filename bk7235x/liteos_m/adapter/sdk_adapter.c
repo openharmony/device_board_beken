fd9f177ba3e0c22876099e3c5b692a6ced6c3390
wenxing.tu <wenxing.tu@bekencorp.com>
2022-12-28 17:48:01 +0800

    fix xts test bug && code check
    Signed-off-by: wenxing.tu <wenxing.tu@bekencorp.com>

--- bk7235x/liteos_m/adapter/sdk_adapter.c
+++ bk7235x/liteos_m/adapter/sdk_adapter.c
@@ -13,8 +13,6 @@
 
 #include <stdio.h>
 #include "cmsis_os2.h"
-#include "ohos_types.h"
-#include "sdkconfig.h"
 #include "platform.h"
 #include "bk_cli.h"
 
@@ -27,52 +25,51 @@ void HalIrqEnable(uint32 irq_no)
 	set_csr(NDS_MIE, (1<<irq_no));
 }
 
-int __wrap_vprintf(const char *fmt, va_list ap)
+void bk_wrap__ctype_ptr__(void)
 {
-	int len;
-	char string[128];
+}
+void HAL_NVIC_SystemReset(void)
+{
+}
 
-	len = __wrap_vsnprintf(string, sizeof(string) - 1, fmt, ap);
-	string[127] = 0;
-	uart_write_string(CONFIG_UART_PRINT_PORT, string);
-	
-	return len;
+#define vprintf_buf_len 128
+int bk_wrap_vprintf(const char *fmt, va_list ap)
+{
+    int len;
+    char string[vprintf_buf_len];
+
+    len = __wrap_vsnprintf(string, sizeof(string) - 1, fmt, ap);
+    string[vprintf_buf_len-1] = 0;
+    uart_write_string(CONFIG_UART_PRINT_PORT, string);
+
+    return len;
 }
 
 __attribute__((section(".interrupt.HalTrapVector.text")))
 void HalSetLocalInterPri(UINT32 interPriNum, UINT16 prior)
 {
-	__nds__plic_set_priority(interPriNum, prior);
+    __nds__plic_set_priority(interPriNum, prior);
 }
 
 static int hdf_log_default_level = 4;
 
 int hal_trace_printf(int attr, const char *fmt, ...)
 {
-	if (attr <= hdf_log_default_level) {
-		va_list ap;
-		int len;
-		char string[128];
+    if (attr <= hdf_log_default_level) {
+        va_list ap;
+        int len;
+        char string[vprintf_buf_len];
 
-		va_start(ap, fmt);
-		len = __wrap_vsnprintf(string, sizeof(string) - 1, fmt, ap);
-		string[127] = 0;
-		uart_write_string(CONFIG_UART_PRINT_PORT, string);
-		va_end(ap);		
-	}
-}
-
-
-unsigned int __stack_chk_guard = 0xd00a0dff;
-
-void __stack_chk_fail(VOID)
-{
-	printf("here you go!!!\n");
-    printf("add is %lu\n", __builtin_return_address(0));
+        va_start(ap, fmt);
+        len = __wrap_vsnprintf(string, sizeof(string) - 1, fmt, ap);
+        string[sizeof(string)-1] = 0;
+        uart_write_string(CONFIG_UART_PRINT_PORT, string);
+        va_end(ap);
+    }
 }
 
 ssize_t _write(int filedes, const void *buf, size_t nbytes)
 {
-	return 0;
+    return 0;
 }
 
